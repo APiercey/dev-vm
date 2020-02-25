f7e56937a260b7938e48371cc4e285bf932c657c
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-02-25 11:10:43 +0100

    Fix java issues

--- provision_scripts/install_java.sh
+++ provision_scripts/install_java.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-source $HOME/.asdf/asdf.sh
+# source $HOME/.asdf/asdf.sh
 
-LANG_VERSION=adopt-openjdk-9+181
+# LANG_VERSION=jdk-10.0.2+13.1
 
-if ! asdf plugin list | grep -q 'java'; then
-  asdf plugin-add java https://github.com/halcyon/asdf-java.git
-fi
+# if ! asdf plugin list | grep -q 'java'; then
+#   asdf plugin-add java https://github.com/halcyon/asdf-java.git
+# fi
 
-if ! asdf where java $LANG_VERSION | grep -q "$LANG_VERSION"; then
-  asdf install java $LANG_VERSION
-fi
+# if ! asdf where java $LANG_VERSION | grep -q "$LANG_VERSION"; then
+#   asdf install java $LANG_VERSION
+# fi
 
-asdf global java $LANG_VERSION
+# asdf global java $LANG_VERSION
+
+
+# ASDF has some serious issues with installing JAVA :(
+sudo apt install default-java
 
